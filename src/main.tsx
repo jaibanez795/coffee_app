ba17d580ced7104af689c53177ff34314b86bb68
Ibañez <klnm638@astrazeneca.net>
2026-06-26 17:31:39 -0600

    LoginPage creado con el template de Materil UI

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { CoffeeApp } from './CoffeeApp.tsx'
+import { LoginPage } from './auth/pages/LoginPage.tsx' 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <CoffeeApp />
+    <LoginPage />
   </StrictMode>
 )

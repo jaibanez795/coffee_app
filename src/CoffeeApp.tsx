f38383189b270bf46f5ed09d771e3a3d1fae742d
Ibañez <klnm638@astrazeneca.net>
2026-06-26 17:06:32 -0600

    Primera estructura de carpetas

--- src/CoffeeApp.tsx
+++ src/CoffeeApp.tsx
@@ -4,7 +4,6 @@ import Button from '@mui/material/Button';
 export const CoffeeApp = () => {
     return (
         <>
-            <div>coffee_app</div>
             <Button variant="contained">Hello world</Button>
         </>
     )

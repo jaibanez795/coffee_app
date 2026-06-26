050c45721a71d52b7fc1148eea2f2d0f1bdfd660
Ibañez <klnm638@astrazeneca.net>
2026-06-26 16:58:19 -0600

    Material UI: instaled

---
+++ src/CoffeeApp.tsx
@@ -0,0 +1,11 @@
+
+import Button from '@mui/material/Button';
+
+export const CoffeeApp = () => {
+    return (
+        <>
+            <div>coffee_app</div>
+            <Button variant="contained">Hello world</Button>
+        </>
+    )
+}
